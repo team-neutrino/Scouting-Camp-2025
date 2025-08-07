76b553e4930885bc31676b57721fd253eecc8960
allencai11 <152452752+allencai11@users.noreply.github.com>
2025-08-07 16:08:35 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+function changeText() {
+    // change the id in the parantheses 
+    const headingElement = document.getElementById("you pick the id");
+    // you can change the replacement text here
+    headingElement.textContent = "Text Changed Using Javascript";
+}
