9416330c7f5cf9b98af637a2837dd3af2efc4be8
ArcherNiemi <kingdedede2019@gmail.com>
2025-09-01 14:31:16 -0500

    cool stuff
    gradient, terminal, buttons

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+ let clickCount = 0;
+var actionList = [""];
+ 
+function addAction(action) {
+    actionList.push(action)
+
+    var jacksonsBlackMagic = actionList.slice().reverse().join("\n");
+    var terminal = document.getElementById("terminal")
+
+    terminal.value = jacksonsBlackMagic
+}
+
 function changeText() {
     // change the id in the parantheses 
-    const headingElement = document.getElementById("you pick the id");
+    const headingElement = document.getElementById("bigButton");
+
     // you can change the replacement text here
-    headingElement.textContent = "Text Changed Using Javascript";
+    clickCount += 1;
+    headingElement.textContent = "that other button was clicked " + clickCount + " times";
+
+    if (clickCount > 20) {
+        headingElement.textContent = headingElement.textContent + " youre a nerd btw"
+    }
 }
