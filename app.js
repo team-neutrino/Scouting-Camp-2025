c0faa40a386d6863e0221a2b5f3c75df8292e9b2
ArcherNiemi <kingdedede2019@gmail.com>
2025-09-08 18:03:25 -0500

    Added some html and css

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function changeText() {
-    // change the id in the parantheses 
+    // change the id in the parantheses
     const headingElement = document.getElementById("you pick the id");
     // you can change the replacement text here
     headingElement.textContent = "Text Changed Using Javascript";
 }
+
