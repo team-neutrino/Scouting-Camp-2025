f35a8f3a67769d321d3daccdb4670d48eeba2e10
Neutrino Highland <neutrino3928@outlook.com>
2025-09-15 19:35:12 -0500

    worked on javascript

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-function changeText() {
+function changeText(id) {
     // change the id in the parantheses
-    const headingElement = document.getElementById("you pick the id");
+    const headingElement = document.getElementById(id);
     // you can change the replacement text here
     headingElement.textContent = "Text Changed Using Javascript";
+
+    for (var i = 0; i < 5; i++) {
+        console.log("hello")
+    }
 }
 
+function clickButton(id, action) {
+    var actionList = []
+
+    actionList.push(action)
+
+    const element = document.getElementById("terminal")
+    console.log("here")
+
+    element.textContent = actionList;
+}
